31635cf48081ed17657c47d5be38cc948c7f74bc
Christopher Pisano <christopher.pisano@capitalone.com>
2017-03-22 15:59:26 -0400

    ruby script to validate CFTs

---
+++ validate_template.rb
@@ -0,0 +1,53 @@
+#!/usr/bin/env ruby
+
+require 'optparse'
+require 'open3'
+require 'ostruct'
+
+@options = OpenStruct.new
+parser = OptionParser.new do |opts|
+  opts.banner = 'Usage: .validate_template [options]'
+
+  opts.on('-p PROFILE', '--profile', 'AWS Credential Profile') do |profile|
+    @options.profile = profile
+  end
+
+  opts.on_tail('-h', '--help', 'print help menu') do |help|
+    puts opts
+    exit
+  end
+end
+parser.parse!
+
+if @options.profile.nil?
+  puts 'no profile specified. will look for environment variables. eg. AWS_DEFAULT_PROFILE or AWS_PROFILE'
+end
+
+def get_files(path)
+  files = Array.new
+
+  if File.dirname(__FILE__).eql?('.')
+    files = Dir.entries(path).select { |f| !File.directory? f}.select { |i| i =~ /\.json|\.yaml|\.yml/ }
+  else
+    files = Dir.glob("#{path}/*").select { |i| i =~ /\.json|\.yaml|\.yml/ }
+  end
+end
+
+def check_valid(file)
+  if @options.profile.nil?
+    cmd = "aws cloudformation validate-template --template-body file://#{file}"
+  else
+    cmd = "aws --profile #{@options.profile} cloudformation validate-template --template-body file://#{file}"
+  end
+
+  Open3.popen3(cmd) do |_, stdout, stderr, _|
+    while err = stderr.gets
+      puts "#{file}: #{err}"
+    end
+  end
+end
+
+get_files(File.dirname(__FILE__)).each do |file|
+  check_valid(file)
+end
+
